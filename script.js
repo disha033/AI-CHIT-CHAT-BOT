1aa5451aa73cb758adda6b853f46a3491f864548
disha033 <lalitamehra020@gmail.com>
2025-03-18 00:27:12 +0530

    Updatee script.js
    api

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let image=document.querySelector("#image img")
 let imageinput=document.querySelector("#image input")
 
 
-const Api_Url="your_api_key"
+const Api_Url="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=your_api_key"
 let user={
     message:null,
     file:{
